2a16264d25d4022376cfc3bf622a38c3b0c0ff69
Jeffrey Flanigan <jflanigan@cs.cmu.edu>
2014-01-21 22:42:10 -0500

    FeatureExtractors

--- src/main/scala/edu/cmu/cs/ark/semeval2014/common/FeatureExtractor1.scala
+++ src/main/scala/edu/cmu/cs/ark/semeval2014/common/FeatureExtractor1.scala
@@ -1,6 +1,6 @@
 package edu.cmu.cs.ark.semeval2014.common
 
-abstract class FeatureExtractor1(input: Input) {
+abstract class FeatureExtractor1(snt: AnnotatedSentence) {
     def features(word: Int) : FeatureVector
 }
 
--- src/main/scala/edu/cmu/cs/ark/semeval2014/common/FeatureExtractor2.scala
+++ src/main/scala/edu/cmu/cs/ark/semeval2014/common/FeatureExtractor2.scala
@@ -1,6 +1,6 @@
 package edu.cmu.cs.ark.semeval2014.common
 
-abstract class FeatureExtractor2(input: Input) {
+abstract class FeatureExtractor2(snt: AnnotatedSentence) {
     def features(word1: Int, word2: Int, label: String) : FeatureVector
 }
 
