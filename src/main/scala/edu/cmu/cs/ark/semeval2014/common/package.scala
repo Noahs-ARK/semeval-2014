7b31c53326947e5d5fea81993ee6bff914abe047
Jeffrey Flanigan <jflanigan@cs.cmu.edu>
2014-01-22 04:10:52 -0500

    Comments

--- src/main/scala/edu/cmu/cs/ark/semeval2014/common/package.scala
+++ src/main/scala/edu/cmu/cs/ark/semeval2014/common/package.scala
@@ -1,6 +1,13 @@
 package edu.cmu.cs.ark.semeval2014
 import scala.language.implicitConversions
 
+// In Java, this package is:
+// public final class edu.cmu.cs.ark.semeval2014.common.package extends java.lang.Object{
+//     public static void logger(int, java.lang.Object);    // print to std err
+//     public static void verbosity_$eq(int);               // set verbosity
+//     public static int verbosity();                       // get current verbosity
+// }
+
 package object common {
     implicit def doubleToMulAssoc(x: Double) = new MulAssoc(x)
     var verbosity = 1
