f820b389afb428afba686215efb7fc477d64169a
Jeffrey Flanigan <jflanigan@cs.cmu.edu>
2014-01-22 01:58:09 -0500

    Fix snt

--- src/main/scala/edu/cmu/cs/ark/semeval2014/common/FeatureExtractor1.scala
+++ src/main/scala/edu/cmu/cs/ark/semeval2014/common/FeatureExtractor1.scala
@@ -5,10 +5,11 @@ package edu.cmu.cs.ark.semeval2014.common
 //  This class translates to Java code:
 //    public abstract class FeatureExtractor1 {
 //      public FeatureExtractor1(AnnotatedSentence);  // Constructor is passed the input annotated sentence
+//      public AnnotatedSentence snt();               // input annotated sentence is member function "snt"
 //      public abstract FeatureVector features(int);  // should compute the features
 //    }
 
-abstract class FeatureExtractor1(snt: AnnotatedSentence) {
+abstract class FeatureExtractor1(val snt: AnnotatedSentence) {
     def features(word: Int) : FeatureVector
 }
 
--- src/main/scala/edu/cmu/cs/ark/semeval2014/common/FeatureExtractor2.scala
+++ src/main/scala/edu/cmu/cs/ark/semeval2014/common/FeatureExtractor2.scala
@@ -5,10 +5,11 @@ package edu.cmu.cs.ark.semeval2014.common
 //  This class translates to Java code:
 //    public abstract class FeatureExtractor1 {
 //      public FeatureExtractor1(AnnotatedSentence);  // Constructor is passed the input annotated sentence
+//      public AnnotatedSentence snt();               // input annotated sentence is member function "snt"
 //      public abstract FeatureVector features(int, int, String);  // should compute the features
 //    }
 
-abstract class FeatureExtractor2(snt: AnnotatedSentence) {
+abstract class FeatureExtractor2(val snt: AnnotatedSentence) {
     def features(word1: Int, word2: Int, label: String) : FeatureVector
 }
 
