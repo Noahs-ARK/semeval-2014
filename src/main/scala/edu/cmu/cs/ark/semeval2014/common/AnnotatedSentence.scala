7819cd344c0eaf4cebb9764188273231783be640
Jeffrey Flanigan <jflanigan@cs.cmu.edu>
2014-01-22 06:35:09 -0500

    Load annotations from corpus

--- src/main/scala/edu/cmu/cs/ark/semeval2014/common/AnnotatedSentence.scala
+++ src/main/scala/edu/cmu/cs/ark/semeval2014/common/AnnotatedSentence.scala
@@ -1,8 +1,11 @@
 package edu.cmu.cs.ark.semeval2014.common
 
-// In Java, this class is:
+import scala.collection.mutable.Map
+
+// In Java, the important members of this class are:
 // public class AnnotatedSentence {
-//     public AnnotatedSentence(String[], Dependency[], String[]);
+//     public AnnotatedSentence(String[], Dependency[], String[]);  // Constructor
+//     public static AnnotatedSentence fromString(String);          // Static constructor
 //     public String[] sentence();
 //     public Dependency[] dependencies();
 //     public String[] pos();
@@ -11,3 +14,45 @@ package edu.cmu.cs.ark.semeval2014.common
 case class AnnotatedSentence(sentence: Array[String],
                              dependencies: Array[Dependency],
                              pos: Array[String])
+
+object AnnotatedSentence {
+
+// fromString loads an annotated from a sentence in this format:
+// Annotations are split with \t###\t, fields are split with \t, and the first field is a marker for the type of annotation
+// Lines beginning with # are ignored.
+// Annotations that can be represented over tokens can be added as comments.
+// Ex:
+// #20001001
+// DEPS    1   NNP 2   nn  ### SDP 1   Pierre  Pierre  NNP -   +   _   _   _   _   _   _   _   _   _   _   _
+// DEPS    2   NNP 9   nsubj   ### SDP 2   Vinken  _generic_proper_ne_ NNP -   -   compound    _   _   ARG1    ARG1    _   _   _   _   _   _
+// DEPS    3   ,   2   punct   ### SDP 3   ,   _   ,   -   -   _   _   _   _   _   _   _   _   _   _   _
+// DEPS    4   CD  5   num ### SDP 4   61  _generic_card_ne_   CD  -   +   _   _   _   _   _   _   _   _   _   _   _
+// DEPS    5   NNS 6   npadvmod    ### SDP 5   years   year    NNS -   +   _   ARG1    _   _   _   _   _   _   _   _   _
+// DEPS    6   JJ  2   amod    ### SDP 6   old old JJ  -   +   _   _   measure _   _   _   _   _   _   _   _
+
+
+    def fromString(string: String) : AnnotatedSentence = {
+        val lines = string.split("\n").filterNot(x => x.matches("^#.*"))
+        val annotations = AnnotatedSentence(new Array(lines.size),
+                                            if (lines.size > 0 && lines(0).matches("""^DEPS\t.*|.*\t###\tDEPS\t.*""")) { new Array(lines.size) } else { new Array(0) },
+                                            new Array(lines.size))
+        for ((line, i) <- lines.zipWithIndex) {
+            val field = splitLine(line)
+            annotations.sentence(i) = field("SDP")(1)
+            annotations.pos(i) = field("SDP")(3)
+            if (field.contains("DEPS")) {
+                annotations.dependencies(i) = Dependency.fromSemEval8(field("DEPS").mkString("\t"))
+            }
+        }
+        return annotations
+    }
+
+    def splitLine(line: String) : Map[String, Array[String]] = {
+        val map : Map[String, Array[String]] = Map()
+        for (split <- line.split("\t###\t")) {
+            map(split.split("\t")(0)) = split.split("\t").tail
+        }
+        return map
+    }
+}
+
--- src/main/scala/edu/cmu/cs/ark/semeval2014/utils/Corpus.scala
+++ src/main/scala/edu/cmu/cs/ark/semeval2014/utils/Corpus.scala
@@ -1,11 +1,18 @@
 package edu.cmu.cs.ark.semeval2014.utils
 
+import edu.cmu.cs.ark.semeval2014.common.AnnotatedSentence
+import scala.io.Source
+
 object Corpus {
     def splitOnNewline(iterator: Iterator[String]) : Iterator[String] = {   // This treats more than one newline in a row as a single newline
         for {
             x <- iterator if x != ""
             p = (x :: iterator.takeWhile(_ != "").toList).mkString("\n")
         } yield p
+    }
+
+    def getAnnotatedSentences(filename: String) : Array[AnnotatedSentence] = {
+        return splitOnNewline(Source.fromFile(filename).getLines).map(x => AnnotatedSentence.fromString(x)).toArray
     }
 }
 
