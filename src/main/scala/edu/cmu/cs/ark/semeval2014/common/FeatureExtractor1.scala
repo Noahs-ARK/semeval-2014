4a332ba97f9d333884a1adee8e57da0bb453c037
Jeffrey Flanigan <jflanigan@cs.cmu.edu>
2014-01-21 23:22:59 -0500

    Comments

--- src/main/scala/edu/cmu/cs/ark/semeval2014/common/FeatureExtractor1.scala
+++ src/main/scala/edu/cmu/cs/ark/semeval2014/common/FeatureExtractor1.scala
@@ -1,5 +1,13 @@
 package edu.cmu.cs.ark.semeval2014.common
 
+// Abstract class for implementing feature extractors for single words
+
+//  This class translates to Java code:
+//    public abstract class FeatureExtractor1 {
+//      public FeatureExtractor1(AnnotatedSentence);  // Constructor is passed the input annotated sentence
+//      public abstract FeatureVector features(int);  // should compute the features
+//    }
+
 abstract class FeatureExtractor1(snt: AnnotatedSentence) {
     def features(word: Int) : FeatureVector
 }
--- src/main/scala/edu/cmu/cs/ark/semeval2014/common/FeatureExtractor2.scala
+++ src/main/scala/edu/cmu/cs/ark/semeval2014/common/FeatureExtractor2.scala
@@ -1,5 +1,13 @@
 package edu.cmu.cs.ark.semeval2014.common
 
+// Abstract class for implementing feature extractors for two words and a dependency label
+
+//  This class translates to Java code:
+//    public abstract class FeatureExtractor1 {
+//      public FeatureExtractor1(AnnotatedSentence);  // Constructor is passed the input annotated sentence
+//      public abstract FeatureVector features(int, int, String);  // should compute the features
+//    }
+
 abstract class FeatureExtractor2(snt: AnnotatedSentence) {
     def features(word1: Int, word2: Int, label: String) : FeatureVector
 }
