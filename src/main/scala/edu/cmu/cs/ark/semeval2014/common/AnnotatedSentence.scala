25d2c6ef16c43cf0b530c13e2392c5c21795d1ab
Jeffrey Flanigan <jflanigan@cs.cmu.edu>
2014-01-22 04:05:46 -0500

    Comments

--- src/main/scala/edu/cmu/cs/ark/semeval2014/common/AnnotatedSentence.scala
+++ src/main/scala/edu/cmu/cs/ark/semeval2014/common/AnnotatedSentence.scala
@@ -1,5 +1,13 @@
 package edu.cmu.cs.ark.semeval2014.common
 
+// In Java, this class is:
+// public class AnnotatedSentence {
+//     public AnnotatedSentence(String[], Dependency[], String[]);
+//     public String[] sentence();
+//     public Dependency[] dependencies();
+//     public String[] pos();
+// }
+
 case class AnnotatedSentence(sentence: Array[String],
                              dependencies: Array[Dependency],
                              pos: Array[String])
