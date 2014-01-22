006903eca5903f0e90732ccadfbd343fffb30e2b
Jeffrey Flanigan <jflanigan@cs.cmu.edu>
2014-01-22 02:34:43 -0500

    Move package members

--- src/main/scala/edu/cmu/cs/ark/semeval2014/common/package.scala
+++ src/main/scala/edu/cmu/cs/ark/semeval2014/common/package.scala
@@ -3,4 +3,6 @@ import scala.language.implicitConversions
 
 package object common {
     implicit def doubleToMulAssoc(x: Double) = new MulAssoc(x)
+    var verbosity = 1
+    def logger(n: Int, s: Any) { if(n<=verbosity) System.err.println(s) }
 }
--- src/main/scala/edu/cmu/cs/ark/semeval2014/package.scala
+++
@@ -1,7 +0,0 @@
-package edu.cmu.cs
-
-package object ark {
-    var verbosity = 1
-    def logger(n: Int, s: Any) { if(n<=verbosity) System.err.println(s) }
-}
-
