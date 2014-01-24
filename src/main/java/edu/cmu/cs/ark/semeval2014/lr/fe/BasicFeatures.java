0b1cf7b551eaf5773fed105d7aad15b6aa82c7a3
Brendan O'Connor <brenocon@gmail.com>
2014-01-24 15:53:38 -0500

    word bigrams with whitelisting for memory savings

--- src/main/java/edu/cmu/cs/ark/semeval2014/lr/fe/BasicFeatures.java
+++ src/main/java/edu/cmu/cs/ark/semeval2014/lr/fe/BasicFeatures.java
@@ -1,28 +1,55 @@
 package edu.cmu.cs.ark.semeval2014.lr.fe;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import util.BasicFileIO;
 import util.U;
 import edu.cmu.cs.ark.semeval2014.common.InputAnnotatedSentence;
 import edu.cmu.cs.ark.semeval2014.lr.fe.FE.FeatureAdder;
 
 public class BasicFeatures extends FE.FeatureExtractor implements FE.TokenFE, FE.EdgeFE {
-
+	
+	Set<String> whitelist;
+	
+	@Override
+	public void initializeAtStartup() {
+		whitelist = new HashSet<>();
+		for (String line : BasicFileIO.openFileLines("resources/train_vocab.txt")) {
+			String[] parts =line.trim().split(" +");
+			int count = Integer.parseInt(parts[0]);
+			if (count >= 100) {
+				whitelist.add(parts[1]);
+			}
+		}
+		
+	}
 	@Override
 	public void features(int word1, int word2, String label, FeatureAdder fa) {
-//		String w1 = sent.sentence()[word1];
-//		String w2 = sent.sentence()[word2];
-//		fa.add(U.sf("lc:bg:%s_%s_%s", w1.toLowerCase(), w2.toLowerCase(), label));
+		String w1 = sent.sentence()[word1].toLowerCase();
+		String w2 = sent.sentence()[word2].toLowerCase();
+		if (whitelist.contains(w1) && whitelist.contains(w2)) {
+			fa.add(U.sf("lc:bg:%s_%s_%s", w1.toLowerCase(), w2.toLowerCase(), label));
+		}
 
 		String p1 = sent.pos()[word1];
 		String p2 = sent.pos()[word2];
 		fa.add(U.sf("pos:bg:%s_%s_%s", p1, p2, label));
+		
+		String dir = word1>word2 ? "dir=i>j" : "dir=i<j";
+		fa.add(dir);
+		fa.add(U.sf("pos:bg:%s_%s_%s_%s", p1, p2, dir, label));
 	}
 
 	@Override
 	public void features(int word1, FeatureAdder fa) {
 		String p1 = sent.pos()[word1];
 		fa.add(U.sf("pos:%s", p1));
-//		String w1 = sent.pos()[word1].toLowerCase();
-//		fa.add("lc:" + w1);
+		
+		String w1 = sent.pos()[word1].toLowerCase();
+		if (whitelist.contains(w1)) {
+			fa.add("lc:" + w1);
+		}
 	}
 
 }
