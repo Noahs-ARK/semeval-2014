fc82abebda0d7276d055b15f89b13df149815702
Sam Thomson <sammthomson@gmail.com>
2014-01-28 10:26:20 -0500

    make java.sh more portable
    was brittle to try to reverse engineer sbt's classpath

--- java.sh
+++ java.sh
@@ -5,20 +5,15 @@ set -eu
 # Invokes 'java' using .class files as compiled by SBT and/or Eclipse,
 # plus flags to stop crashes due to idiotic default JVM behaviors
 
-CP=target/scala-2.10/classes
-CP=$HOME/.sbt/boot/scala-2.10.3/lib/scala-library.jar:$CP
-
-# Local jars
-CP=lib/sdp.jar:$CP
-CP=lib/myutil.jar:$CP
-CP=lib/kryo-2.22-all.jar:$CP
-
 # file.encoding necessary for mac (ugh!!) http://stackoverflow.com/questions/361975/setting-the-default-java-character-encoding
+# TODO: should set encoding explicitly in the java/scala code
 # XX:ParallelGCThreads prevents horrible crashes on large multicore machines (basically, another stupid java bug)
+# -ea enables assertions
 
-klass=$1
+# so we don't have to type out "edu.cmu.cs.ark.semeval2014." every time
+klass="edu.cmu.cs.ark.semeval2014.$1"
 shift
-klass=edu.cmu.cs.ark.semeval2014.$klass
 
-# set -x
-exec java -ea -Dfile.encoding=UTF-8 -XX:ParallelGCThreads=2 -Xmx4g -cp "$CP" $klass "$@"
+# TODO: instead of using "sbt run-main ...", it's probably easier to package into uberjar then "java -cp uberjar.jar ..."
+# make sure your bin/sbt doesn't override these JAVA_OPTS, or just include these in bin/sbt
+JAVA_OPTS="-ea -Dfile.encoding=UTF-8 -XX:ParallelGCThreads=2 -Xmx4g" sbt "run-main $klass $*"
