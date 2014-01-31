cf0c502db62b51e54528a9f65b4ff4ad7992eedc
Manaal Faruqui <manaalfar@gmail.com>
2014-01-31 11:49:14 -0500

    fix eval.sh script to point at sbt in repo

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -2,4 +2,4 @@
 # second arg: pred
 
 # sbt logs to stdout, and Evaluator prints to stderr. we only care about Evaluator
-sbt "run-main sdp.tools.Evaluator ${1} ${2}" 3>&1 1>/dev/null 2>&3
+`dirname $0`/sbt "run-main sdp.tools.Evaluator ${1} ${2}" 3>&1 1>/dev/null 2>&3
