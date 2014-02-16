52d7270c118fd9872c38a091acfa6fe6d10bd066
Jeffrey Flanigan <jflanigan@cs.cmu.edu>
2014-02-16 18:37:08 -0500

    Distance intervals and log distance

--- src/main/java/edu/cmu/cs/ark/semeval2014/lr/fe/LinearOrderFeatures.java
+++ src/main/java/edu/cmu/cs/ark/semeval2014/lr/fe/LinearOrderFeatures.java
@@ -15,6 +15,6 @@ public class LinearOrderFeatures extends FE.FeatureExtractor implements FE.EdgeF
 		int dist = srcTokenIdx - destTokenIdx;
 		fa.add(U.sf("lin:%s", dist));
 		fa.add(U.sf("left:%s", (srcTokenIdx < destTokenIdx)));
-        fa.add(U.sf("logD:%s", signum(dist)*floor(log(abs(dist)+1)/log(1.39))));
+        fa.add(U.sf("logD:%s", Math.signum(dist)*Math.floor(Math.log(Math.abs(dist)+1)/Math.log(1.39))));
 	}
 }
