dc4faac715ec38c69e9849c52ccce1b6d2b2ae57
Jeffrey Flanigan <jflanigan@cs.cmu.edu>
2014-02-16 18:07:52 -0500

    Distance intervals and log distance

---
+++ src/main/scala/edu/cmu/cs/ark/semeval2014/lr/fe/DistanceIntervals.scala
@@ -0,0 +1,24 @@
+package edu.cmu.cs.ark.semeval2014.lr.fe
+
+import edu.cmu.cs.ark.semeval2014.common.InputAnnotatedSentence
+import edu.cmu.cs.ark.semeval2014.lr.fe.FE.FeatureAdder
+import java.lang.Math.abs
+import java.lang.Math.log
+import java.lang.Math.min
+import java.lang.Math.sign
+import java.lang.Math.floor
+
+class DistanceIntervals extends FE.FeatureExtractor with FE.EdgeFE {
+    override def features(word1Index: Int, word2Index: Int, fa: FeatureAdder) {
+        val distance = abs(srcTokenIdx - destTokenIdx).toInt
+        val direction = sign(dist).toInt
+        // distance intervals
+        for (i <- Range(1, distance-1)) {
+            fa.add("Di="+(direction*i).toString)
+        }
+        // log distance intervals
+        for (i <- Range(1, floor(log(distance+1)/log(1.39)).toInt)) {
+            fa.add("logDi="+(direction*i).toString)
+        }
+    }
+}
