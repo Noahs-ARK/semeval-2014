bcd212152fb5b263c54110d184e8314cdd34696b
Brendan O'Connor <brenocon@gmail.com>
2014-02-28 15:42:32 -0500

    verbose eval by default

--- scripts/verboseeval.sh
+++
@@ -1,5 +0,0 @@
-# first arg: gold
-# second arg: pred
-
-# sbt logs to stdout, and Evaluator prints to stderr. we only care about Evaluator
-`dirname $0`/sbt "run-main sdp.tools.Evaluator ${1} ${2}" 3>&1 1>/dev/null 2>&3
