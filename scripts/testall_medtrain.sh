8ae8656a9815de9ccc30e69fb5b9679cbf46c97c
David Bamman <david.bamman@gmail.com>
2014-03-03 12:01:25 -0500

    removed my med_train scripts, which duplicate med_traintest.sh

--- scripts/testall_medtrain.sh
+++
@@ -1,5 +0,0 @@
-FEATNAME=$1
-
-./scripts/traintest_example_med.sh pas feats/$FEATNAME/pas
-./scripts/traintest_example_med.sh dm feats/$FEATNAME/dm
-./scripts/traintest_example_med.sh pcedt feats/$FEATNAME/pcedt
