a42efe91561f77beea2fdb2592c3991ea23fa58b
Jeffrey Flanigan <jflanigan@cs.cmu.edu>
2014-03-19 20:33:23 -0400

    Fix bug in cost augmented decoding

--- src/main/scala/edu/cmu/cs/ark/semeval2014/amr/GraphDecoder/CostAugmented.scala
+++ src/main/scala/edu/cmu/cs/ark/semeval2014/amr/GraphDecoder/CostAugmented.scala
@@ -13,6 +13,7 @@ import scala.util.matching.Regex
 import scala.collection.mutable.Map
 import scala.collection.mutable.Set
 import scala.collection.mutable.ArrayBuffer
+import edu.cmu.cs.ark.semeval2014.common.FastFeatureVector._
 
 class CostAugmented(val decoder: Decoder, costScale: Double) extends Decoder {
     val features = decoder.features
@@ -22,9 +23,16 @@ class CostAugmented(val decoder: Decoder, costScale: Double) extends Decoder {
         val oracleDecoder = new Oracle(decoder.features.featureNames,       // "CostAugEdgeId" already in featureNames
                                        decoder.features.weights.labelset)
         val oracle = oracleDecoder.decode(input)
-        features.weights -= costScale * oracle.features.filter(x => x.startsWith("CA:"))
+        val addCost = oracle.features.filter(x => x.startsWith("CA:"))
+        for ((feat, values) <- addCost.fmap) {
+            values.unconjoined = 1.0
+            values.conjoined = Map()
+        }
+        features.weights += costScale * addCost  // add costScale to edge weights that don't match oracle (penalie precision type errors)
+        features.weights -= (2*costScale) * oracle.features.filter(x => x.startsWith("CA:")) // subtract costScale from ones that match (penalize recall type errors)
         val result = decoder.decode(Input(input.inputAnnotatedSentence, input.graph.duplicate.clearEdges))
-        features.weights += costScale * oracle.features.filter(x => x.startsWith("CA:"))
+        features.weights -= costScale * addCost  // undo the changes
+        features.weights += (2*costScale) * oracle.features.filter(x => x.startsWith("CA:"))
         val feats = result.features.filter(x => !x.startsWith("CA:"))
         return DecoderResult(result.graph, feats, features.weights.dot(feats))
     }
