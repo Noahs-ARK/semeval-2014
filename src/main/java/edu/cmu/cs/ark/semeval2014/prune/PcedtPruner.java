3c1e7d6b83d44e249b4afa39ef75b919dec60bb5
Swabha Swayamdipta <swabha.swayamdipta@gmail.com>
2014-03-27 11:46:59 -0400

    pre-processed, post-processed, hopefully bugless

--- src/main/java/edu/cmu/cs/ark/semeval2014/prune/PcedtPruner.java
+++ src/main/java/edu/cmu/cs/ark/semeval2014/prune/PcedtPruner.java
@@ -23,7 +23,7 @@ public class PcedtPruner {
     /**
      * Pre-processing step on graphMatrices - converts pcedt formalism into tree
      */
-    public static Graph modifyGraph(Graph g) {
+    public static Graph preprocessOld(Graph g) {
         Graph newGraph = new Graph(g.id);
 
         // map between and parent and its children
@@ -115,18 +115,16 @@ public class PcedtPruner {
     // 1. add two edges to the children of and, from the parent of and
     // 2. if and is the top node, make its children the top nodes
     public static Graph postProcess(Graph g) {
-        Graph newGraph = new Graph(g.id);
-
-        // change top node
+        
+    	// change top node
         int oldTop = -100;
         List<Integer> newTops = new ArrayList<>();
-
-        // and with 2 or more children - fix
 
         // new edges to be added:
         Map<Integer, String> newEdges = new HashMap<>();
         // edges to be removed:
         List<Integer> edgesForRemoval = new ArrayList<>();
+        
         for (Node andNode : g.getNodes()) {
             List<Integer> memberChildren = new ArrayList<>();
             for (Edge outgoing : andNode.getOutgoingEdges()) {
@@ -140,25 +138,56 @@ public class PcedtPruner {
                 continue;
             }
 
-//            System.out.println("incoming to and: "
-//                    + andNode.incomingEdges.size());
-
             // changing the top to others
             if (andNode.incomingEdges.size() == 0 && andNode.isTop) {
                 oldTop = andNode.id;
                 newTops.addAll(memberChildren);
             }
 
+            // case 2: member child is not and; parent is and
+            if (andNode.incomingEdges.size() == 1 && andNode.incomingEdges.get(0).label.contains("member")) {
+            	Edge inEdge = andNode.incomingEdges.get(0);
+            	if (g.getNode(inEdge.source).incomingEdges.size() > 0) { // when does this happen?
+            		int newParent = g.getNode(inEdge.source).incomingEdges.get(0).source;
+                	String newLabel = g.getNode(inEdge.source).incomingEdges.get(0).label;
+                	for (int memberChild : memberChildren) {
+                		newEdges.put(memberChild, newParent + "_" + newLabel);
+                	}
+                	continue;
+                	//nothing to remove
+            	}
+            }
+                       
             for (Edge incoming : andNode.incomingEdges) {
-                edgesForRemoval.add(incoming.id);
+            	// check if member child is and
+            	int newParent;
                 for (int memberChild : memberChildren) {
-                    newEdges.put(memberChild, incoming.source + "_"
-                            + incoming.label);
+                	boolean memberChildIsAnd = false;
+                	for (Edge out : g.getNode(memberChild).outgoingEdges) {
+                		if (out.label.contains("member")) {
+                			memberChildIsAnd = true;
+                			break;
+                		}
+                	}
+                	newParent = incoming.source;
+                	// case 3: member child is not and; parent is not and
+                	if (memberChildIsAnd == false) {
+                		newEdges.put(memberChild, newParent + "_"
+                                + incoming.label);
+                	}  else {
+                		// case 1: member child is and, parent is not and
+                		// do nothing - add no new edge here
+                		continue;
+                	}
                 }
+                edgesForRemoval.add(incoming.id);
+                
+                // case 4: member child is and; parent is and
+                //System.out.println("Go to hell, pcedt!");
             }
         }
 
-
+        Graph newGraph = new Graph(g.id);
         for (Node n : g.getNodes()) {
             if (n.id == oldTop) {
                 newGraph.addNode(n.form, n.lemma, n.pos, false, n.isPred);
@@ -169,6 +198,86 @@ public class PcedtPruner {
             }
         }
 
+        // add all original edges
+        for (Edge e : g.getEdges()) {
+            if (edgesForRemoval.contains(e.id)) {
+                continue;
+            }
+            newGraph.addEdge(e.source, e.target, e.label);
+        }
+        // add all new edges
+        for (int target : newEdges.keySet()) {
+            String[] es = newEdges.get(target).split("_");
+
+            int source = Integer.parseInt(es[0]);
+            String label = es[1];
+            newGraph.addEdge(source, target, label);
+        }
+        return newGraph;
+    }
+    
+    /**
+     * 
+     * @param g
+     * @return
+     */
+    public static Graph preprocess(Graph g) {
+    	// children of and which are also tops
+        List<Integer> topNodes = new ArrayList<>();
+
+        // and parent which should now be the top instead
+        int newTopNode = -100;
+    	
+    	// edges to be added:    	
+    	Map<Integer, String> newEdges = new HashMap<>();
+        // edges to be removed:
+        List<Integer> edgesForRemoval = new ArrayList<>();
+    	
+    	for (Node andNode : g.getNodes()) {
+    		List<Integer> memberChildren = new ArrayList<>();
+    		boolean isAnd = false;
+            for (Edge outgoing : andNode.getOutgoingEdges()) {
+                if (outgoing.label.contains("member")) {
+                    memberChildren.add(outgoing.target);
+                    if (g.getNode(outgoing.target).isTop) {
+                    	topNodes.add(outgoing.target);
+                    	newTopNode = andNode.id;
+                    }
+                    isAnd = true;
+                }
+            }
+            if (isAnd == false) { continue; }
+            if (memberChildren.size() < 2) { // not sure about this
+            	continue;
+            }
+            
+            for (int andChild : memberChildren) {
+        		for (Edge incomingToAndChild : g.getNode(andChild).incomingEdges) {
+        			if (incomingToAndChild.label.contains("member")) { continue; }
+        			edgesForRemoval.add(incomingToAndChild.id);
+        			if (andNode.incomingEdges.size()==1 && andNode.incomingEdges.get(0).label.contains("member")) {
+        				continue; // no new edges to be added
+        			}
+        			int newParent = incomingToAndChild.source;
+        			String newLabel = incomingToAndChild.label;
+        			newEdges.put(andNode.id, newParent+"_"+newLabel);
+        		}
+        	}
+    	}
+    	
+    	Graph newGraph = new Graph(g.id);
+    	// add all nodes to the new graph
+        // node id matters because we are going to add corresponding edges
+        for (Node n : g.getNodes()) {
+            if (topNodes.contains(n.id)) {
+                newGraph.addNode(n.form, n.lemma, n.pos, false, n.isPred);
+            } else if (n.id == newTopNode) {
+                newGraph.addNode(n.form, n.lemma, n.pos, true, n.isPred);
+            } else {
+                newGraph.addNode(n.form, n.lemma, n.pos, n.isTop, n.isPred);
+            }
+        }
+        
         // add all original edges
         for (Edge e : g.getEdges()) {
             if (edgesForRemoval.contains(e.id)) {
--- src/main/java/edu/cmu/cs/ark/semeval2014/util/GenerateGraphsAndVocab.java
+++ src/main/java/edu/cmu/cs/ark/semeval2014/util/GenerateGraphsAndVocab.java
@@ -5,6 +5,7 @@ import edu.cmu.cs.ark.semeval2014.prune.PcedtPruner;
 import sdp.graph.Edge;
 import sdp.graph.Graph;
 import sdp.io.GraphReader;
+import sdp.io.GraphWriter;
 //import sdp.io.GraphWriter;
 import util.Vocabulary;
 
@@ -22,29 +23,26 @@ public class GenerateGraphsAndVocab {
 
         final List<Graph> modifiedGraphs = new ArrayList<>();
         if (doPcedtTreeProcess) {
-//            GraphWriter gw = new GraphWriter("pcedt1.conll");
+            GraphWriter gw = new GraphWriter("pcedt1.conll");
             for (Graph g : graphs) {
-                Graph ng = PcedtPruner.modifyGraph(g);
-//                gw.writeGraph(ng);
+                Graph ng = PcedtPruner.preprocess(g);
+                gw.writeGraph(ng);
                 modifiedGraphs.add(ng);
             }
-//            gw.close();
+            gw.close();
             graphs = modifiedGraphs;
         }
-//        List<Graph> originalGraphs = new ArrayList<Graph>();
-//        gw = new GraphWriter("pcedt2.conll");
-//        int g = 0;
-//        for (Graph ng : modifiedGraphs) {
-//            System.out.println(g + "\n------------------");
-//            Graph og = PcedtPruner.postProcess(ng);
-//            gw.writeGraph(og);
-//            originalGraphs.add(og);
-//            g += 1;
-//        }
-//        gw.close();
+        List<Graph> originalGraphs = new ArrayList<Graph>();
+        GraphWriter gw = new GraphWriter("pcedt2.conll");
+        
+        for (Graph ng : modifiedGraphs) {
+            Graph og = PcedtPruner.postProcess(ng);
+            gw.writeGraph(og);
+            originalGraphs.add(og);
+        }
+        gw.close();
 
-//        System.out.println("done!!!\n");
-//        System.exit(0);
+
 		labelVocab = generateLabelVocab(graphs);
 		graphMatrices = convertGraphsToAdjacencyMatrices(graphs);
 	}
